791b1a48dda42d5e3187dcb20ed458732ccf87ee
baijiayong <jiayongbai@gmail.com>
2014-04-22 00:22:56 +0800

    新增了Member.java 类用于存放member的数据

---
+++ src/main/java/com/bodejidi/Member.java
@@ -0,0 +1,26 @@
+package com.bodejidi;
+
+public class Member
+{
+    private String firstName;
+    private String lastName;
+    private Ingeger id;
+    
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    public void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    public String getLastName()
+    {
+        return lastName;
+    }
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    
+}
