4809b3eca166f9742fefbfb892ab222a8ebe5fca
baijiayong <jiayongbai@gmail.com>
2014-04-24 10:24:25 +0800

    在 MemberDao 中添加了显示会员的 show() 方法

--- src/main/java/com/bodejidi/MemberDao.java
+++ src/main/java/com/bodejidi/MemberDao.java
@@ -2,6 +2,9 @@ package com.bodejidi;
 
 import java.util.Date;
 import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MemberDao
 {
@@ -14,7 +17,6 @@ public class MemberDao
         
         DatabaseManager dbManager = null;
         String sql = "INSERT INTO member(first_name,last_name,created_date,last_updated) VALUES (?,?,?,?)";
-        debug(sql);
 
         try
         {
@@ -33,6 +35,41 @@ public class MemberDao
             dbManager.close();
         }  
         return member;
+    }
+    
+    public List<Member> show()
+    {
+        String sql = "SELECT * FORM member";
+        List<Member> memberList = new ArrayList<Member>();
+        
+        DatabaseManager dbManager = null;
+        
+        Member member = new Member();
+        
+        try
+        {
+            dbManager = DatabaseManager.newInstance();
+            ResultSet rs =  dbManager.executeQuery(sql);
+            debug(sql);
+            while(rs.next())
+            {
+                member.setId(rs.getLong("id"));
+                member.setFirstName(rs.getString("first_name"));
+                member.setLastName(rs.getString("last_name"));
+                memberList.add(member);
+            }
+            
+        }catch(SQLException ex)
+        {
+            debug("SQLExcetion: " + ex.getMessage());
+            debug("SQLState: " + ex.getSQLState());
+            debug("VendorError: " + ex.getErrorCode());
+            debug("Error!");
+        }finally
+        {
+            dbManager.close();
+        }
+        return memberList;
     }
     
     public void debug(String str)
