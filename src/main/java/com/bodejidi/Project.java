b88c8668f146c4f3d03272c2fec7c95b3ae4d2c8
baijiayong <jiayongbai@gmail.com>
2014-04-24 10:21:42 +0800

    将显示会员的方法，添加到 Servlet 的 doGet 中

--- src/main/java/com/bodejidi/Project.java
+++ src/main/java/com/bodejidi/Project.java
@@ -4,16 +4,29 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.RequestDispatcher;
 import java.io.IOException;
 
 public class Project extends HttpServlet
 {
     public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException , ServletException
     {
-        resp.getWriter().println("I am a boy!");
+        String action = req.getParameter("action");
+        
+        Member member = new Member();
+        
+        if ("show".equalsIgnoreCase(action))
+        {
+           MemberDao memberDao = new MemberDao();
+           memberDao.show();
+           req.setAttribute("member",member);
+           forward("show",req,resp);
+        }
     }
     public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
     {
+        Member member = new Member();
+        
         String firstName = req.getParameter("firstName");
         String lastName = req.getParameter("lastName");
        
@@ -21,16 +34,15 @@ public class Project extends HttpServlet
 
         if("add".equalsIgnoreCase(action))
         {
-            Member member = new Member();
             member.setFirstName(firstName);
             member.setLastName(lastName);
             MemberDao memberDao = new MemberDao();
             memberDao.save(member);
-        }else if ("show".equalsIgnoreCase(action))
-        {
-           MemberDao memberDao = new MemberDao();
-           memberDao.show();
-           
         }
+    }
+    public void forward(String page, HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException
+    {
+        String jsp = "/WEB-INF/member/" + page +".jsp";
+        getServletContext().getRequestDispatcher(jsp).forward(req,resp);
     }
 }
